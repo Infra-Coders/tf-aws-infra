691813dd99e8e5df98f77ccf6cb5e829c5d25395
Michal Oliwiak <olioli@tlen.pl>
2025-11-25 15:25:23 +0100

    Add dynamic SSH key generation

--- single-master-k8s/provider.tf
+++ single-master-k8s/provider.tf
@@ -3,3 +3,20 @@ provider "aws" {
   #access_key = <aws_access_key_id>
   #secret_key = <aws_secret_access_key>
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.22"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
