cce2fb5e444839d7a41ffae06c0c54cb14218ebb
Maciej Krawczyszyn <maciej.krawczyszyn@gmail.com>
2026-02-10 22:06:44 +0100

    Updated to the most recent AMI release of Ubuntu

--- single-master-k8s/ami.tf
+++ single-master-k8s/ami.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20250516"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20260203"]
   }
 
   filter {
